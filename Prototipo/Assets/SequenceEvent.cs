fb843ff24b512888430d7b48067a9a033057879c
matheuspb95@gmail.com <matheuspb95@gmail.com>
2023-08-28 18:33:07 -0400

    Primeiros passos, narração, pegar EPIs

---
+++ Prototipo/Assets/SequenceEvent.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public struct Step {
+    public AudioClip Narration;
+    public float duration;
+    public UnityEvent onEnd; 
+}
+
+public class SequenceEvent : MonoBehaviour
+{
+    public List<Step> steps; 
+    public int actualStep = 0;
+    public AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        StartCoroutine(NextStep());
+    }
+
+    public void CallNextStep(){
+        actualStep++;
+        StartCoroutine(NextStep());
+    }
+
+    IEnumerator NextStep() {
+        if(steps[actualStep].Narration != null){
+            audio.clip = steps[actualStep].Narration;
+            audio.Play();
+            yield return new WaitForSeconds(audio.clip.length);
+            steps[actualStep].onEnd.Invoke();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
