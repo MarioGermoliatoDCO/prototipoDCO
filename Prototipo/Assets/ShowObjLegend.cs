baddb7f3eed5407b1ec295cc3ece23d7d86d06ab
matheuspb95@gmail.com <matheuspb95@gmail.com>
2023-09-14 14:38:24 -0400

    Legendas Obj

--- Prototipo/Assets/ShowObjLegend.cs
+++ Prototipo/Assets/ShowObjLegend.cs
@@ -9,15 +9,21 @@ public class ShowObjLegend : MonoBehaviour
     private Camera mainCamera;
     public Vector3 offset;
     // Start is called before the first frame update
+    private LineRenderer line;
+    private Vector3 parentCenter;
     void Start()
     {
         mainCamera = Camera.main;
+        line = GetComponent<LineRenderer>();
+        // line.SetPositions(new Vector3[] {this.transform.position, this.transform.parent.parent.position});
+        parentCenter = this.transform.parent.parent.GetComponent<Collider>().bounds.center;
+        line.SetPositions(new Vector3[] {this.transform.position, parentCenter});
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.localScale = Vector3.one * distanceMultiplier * Vector3.Distance(this.transform.position, mainCamera.transform.position);
+        // this.transform.localScale = Vector3.one * (0.1f + distanceMultiplier) * (Vector3.Distance(this.transform.position, mainCamera.transform.position) * 0.5f);
     }
 
     public void ShowLegend(GameObject obj)
